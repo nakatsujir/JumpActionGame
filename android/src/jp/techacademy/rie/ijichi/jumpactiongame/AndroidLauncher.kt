b010cac2a84386e998ac3935b29c14f13150ca67
nakatsuji <rie.nakatsuji1123@gmail.com>
2019-07-03 21:47:15 +0900

    Initial commit

---
+++ android/src/jp/techacademy/rie/ijichi/jumpactiongame/AndroidLauncher.kt
@@ -0,0 +1,15 @@
+package jp.techacademy.rie.ijichi.jumpactiongame
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import jp.techacademy.rie.ijichi.jumpactiongame.JumpActionGame
+
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(JumpActionGame(), config)
+    }
+}
