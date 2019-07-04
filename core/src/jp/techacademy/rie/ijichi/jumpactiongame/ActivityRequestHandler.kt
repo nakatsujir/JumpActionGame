76fe097453ba978c3380f4f31311d73668f1bb1e
nakatsuji <rie.nakatsuji1123@gmail.com>
2019-07-04 19:20:09 +0900

    Lesson7実装

---
+++ core/src/jp/techacademy/rie/ijichi/jumpactiongame/ActivityRequestHandler.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.rie.ijichi.jumpactiongame
+
+interface ActivityRequestHandler {
+    fun showAds(show:Boolean)
+}
--- core/src/jp/techacademy/rie/ijichi/jumpactiongame/JumpActionGame.kt
+++ core/src/jp/techacademy/rie/ijichi/jumpactiongame/JumpActionGame.kt
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-class JumpActionGame : Game() {
+class JumpActionGame (val mRequestHander:ActivityRequestHandler): Game() {
     lateinit var batch: SpriteBatch
 
     override fun create() {
